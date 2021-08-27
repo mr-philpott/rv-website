764ac8a0b074d70dc27270df8fafb16a8e6d002b
Ethan Philpott <ephilpott2004@gmail.com>
2021-08-26 23:48:59 -0700

    Worked on one tempoary design for the home page, definitly not set in stone

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import rvv2 from "./assets/images/rvv2.png"
+
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <img src={rvv2} alt="rv"/>
+      <div className="cover">
+
+      </div>
       
     </div>
   );
