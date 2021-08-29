61f7054abd18743c08d72bb5f1a15840a107d61d
Ethan Philpott <ephilpott2004@gmail.com>
2021-08-28 22:40:59 -0700

    worked on navbar

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,29 +1,71 @@
+import { useState } from "react"
+
 const Nav = () => {
+    const [dropdown, setDropdown] = useState(false);
+
     return (
-        <nav>
-            <div className="nav-branding">
-                <div className="logo"></div>
-                <div className="company-name">RV Inspection and Repair Services</div>
+        <>
+            <nav>
+                <div className="nav-branding">
+                    <div className="logo">
+                    </div>
+                    <div className="company-name">RV / AC Inspection and Repair</div>
+                </div>
+                <ul className="links">
+                    <li>
+                        <a href="#services">
+                            Services
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#gallery">
+                            Gallery
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#review">
+                            Review
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#gallery">
+                            Contact
+                        </a>
+                    </li>
+                </ul>
+                <div className="nav-toggle">
+                    <div className={dropdown ? "cross" : "hamburger"} onClick={() => setDropdown(!dropdown)}>
+                        <div className="slice"></div>
+                        <div className="slice"></div>
+                        <div className="slice"></div>
+                    </div>
+                </div>
+            </nav>
+            <div className={`nav-dropdown ${dropdown ? "show" : "hide"}`}>
+                <ul className="links">
+                    <li>
+                        <a href="#services">
+                            Services
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#gallery">
+                            Gallery
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#review">
+                            Review
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#gallery">
+                            Contact
+                        </a>
+                    </li>
+                </ul>
             </div>
-            <ul className="links">
-                <li>
-                    <a href="#services">
-                        Services
-                    </a>
-                </li>
-                <li>
-                    <a href="#gallery">
-                        Gallery
-                    </a>
-                </li>
-                <li>
-                    <a href="#gallery">
-                        Contact
-                    </a>
-                </li>
-            </ul>
-            {/* <div className="hamburger"></div> */}
-        </nav>
+        </>
     )
 }
 
