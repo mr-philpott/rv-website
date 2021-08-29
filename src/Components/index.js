7b098d9a02143430c1e30b945f05271e5bdace4d
Ethan Philpott <ephilpott2004@gmail.com>
2021-08-28 20:51:52 -0700

    Added new wireframe images

--- src/Components/index.js
+++ src/Components/index.js
@@ -2,3 +2,10 @@ import Footer from "./Footer";
 import Home from "./Home";
 import Nav from "./Nav";
 import Services from "./Services";
+
+export {
+    Footer,
+    Home,
+    Nav,
+    Services
+}
