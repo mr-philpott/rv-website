177783ffa4d228a62361ef32bef2358982f6b460
Ethan Philpott <Ephilp288@west-mec.local>
2021-09-01 17:22:43 -0700

    Wokred on sizing some images on scroll, something no one will notice

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useScroll } from '../contexts/ScrollContext'
+
+const minZoom = 1;
+const maxZoom = 1.5;
+let valZoom = minZoom;
+const scrollStart = 175;
+const scrollEnd = 600;
 
 const Services = () => {
     const scroll = useScroll();
+
+    useEffect(() => {
+        valZoom = 
+            scroll > scrollStart ? 
+                scroll > scrollEnd ? 
+                    maxZoom :
+                    minZoom + (maxZoom - minZoom) * (scroll / scrollEnd)
+                : minZoom;
+    }, [scroll])
 
     return (
         <>
@@ -14,14 +29,18 @@ const Services = () => {
                             <h3>RV Inspection</h3>
                             <p>I inspect RVs, providing insight into how the RVs are currently feeling. I think I’m a bit of an RV therapist myself.</p>
                         </div>
-                        <img src="https://picsum.photos/500/600" alt="Inspection Image" style={{objectPosition: `0 ${scroll / 2 - 300}px`}}/>
+                        <div className="img-container">
+                            <img src="https://picsum.photos/500/600" alt="Inspection Image" style={{transform: `scale(${valZoom})`}}/>
+                        </div>
                     </div>
                     <div className="service-section repair">
                         <div className="info">
                             <h3>AC Repair</h3>
                             <p>I can repair air conditioning units, unless they are running, in which case I’ll die from electrical shock. But anyways, I can still do it!</p>
                         </div>
-                        <img src="https://picsum.photos/500/601" alt="Repair Image" style={{objectPosition: `0 ${scroll / 2 - 300}px`}}/>
+                        <div className="img-container">
+                            <img src="https://picsum.photos/500/601" alt="Repair Image" style={{transform: `scale(${valZoom})`}}/>
+                        </div>
                     </div>
                 </div>
             </section>
