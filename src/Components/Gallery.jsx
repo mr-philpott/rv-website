9daffeb90d2c6dc506a74611aeb4e9d4def3f584
Alisson <alisson.corrales24@gmail.com>
2021-09-02 14:27:12 -0700

    fixing gallery for desktop

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -7,20 +7,34 @@ const Gallery = () => {
             
             <h3>Gallery</h3>
 
-            <div className="gallery-grid">
+            <div className="gallery-mobile">
                 <div className="cols">
                     <div className="row1">
-                        <img src="https://picsum.photos/500/900" alt="example" id="img-big1square"/>
-                        <img src="https://picsum.photos/500/650" alt="example" id="img-square1"/>
+                        <img src="https://picsum.photos/600/800" alt="example" id="img-big1square"/>
+                        <img src="https://picsum.photos/600/650" alt="example" id="img-square1"/>
                     </div>
                     <div className="row2">
-                        <img src="https://picsum.photos/500/650" alt="example" id="img-square2"/>
-                        <img src="https://picsum.photos/500/900" alt="example" id="img-big2square"/>
+                        <img src="https://picsum.photos/600/650" alt="example" id="img-square2"/>
+                        <img src="https://picsum.photos/600/800" alt="example" id="img-big2square"/>
                     </div>
                 </div>
                 <div className="row3">
-                        <img src="https://picsum.photos/500/400" alt="example" id="img-long"/>
+                        <img src="https://picsum.photos/500/300" alt="example" id="img-long"/>
                     </div>
+            </div>
+
+            <div className="gallery-desk">
+                <div className="row0">
+                    <img src="https://picsum.photos/400/450" alt="example" id="img-big1square"/>
+                    <img src="https://picsum.photos/600/450" alt="example" id="img-square1"/>
+                </div>
+                <div className="row1">
+                    <img src="https://picsum.photos/1000/400" alt="example" id="img-long"/>
+                </div>
+                <div className="row2">
+                    <img src="https://picsum.photos/400/450" alt="example" id="img-big1square"/>
+                    <img src="https://picsum.photos/600/450" alt="example" id="img-square1"/>
+                </div>
             </div>
         </section>
     )
